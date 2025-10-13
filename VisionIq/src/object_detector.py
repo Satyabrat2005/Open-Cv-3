39922fc6adfea1c5fc81533c6621ff5e8e07730d
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-13 23:39:55 +0530

    Update object_detector.py

--- VisionIq/src/object_detector.py
+++ VisionIq/src/object_detector.py
@@ -1,1 +1,11 @@
+from ultralytics import YOLO
+model = YOLO("models/yolov8n.pt")
 
+def detetct_objects(frame_path):
+    results = model.predict(frame_path)
+    objects = []
+    for r in results:
+        for box in r.boxes:
+            cls_id = int(box.cls[0])
+            conf = float(box.conf[0])
+            objects.append((cls_id, conf))
