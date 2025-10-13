c7e4190b989ece94ebe6b0d5ac44dd1c09d876b7
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-14 00:31:24 +0530

    Update database.py

--- VisionIq/src/database.py
+++ VisionIq/src/database.py
@@ -6,24 +6,15 @@ import pickle
 class VectorDB:
     def __init__(self, dim, db_path='vector_db'):
         self.dim = dim
-        self.db_path = db_path
-        self.index_file = os.path.join(db_path, 'faiss_index.bin')
-        self.meta_file = os.path.join(db_path, 'metadata.pkl')
-        os.makedirs(db_path, exist_ok=True)
+        self.index_file = index_file
+        self.index = faiss.IndexFlatL2(dim)
+        self.metadata = []
+        if os.path.exists(index_file):
+            self.load()
 
-        if os.path.exists(self.index_file) and os.path.exists(self.meta_file):
-            self.index = faiss.read_index(self.index_file)
-            with open(self.meta_file, 'rb') as f:
-                self.metadata = pickle.load(f)
-        else:
-            self.index = faiss.IndexFlatL2(dim)
-            self.metadata = []
-
-    def add_vectors(self, vectors, meta):
-        vectors = np.array(vectors).astype('float32')
-        self.index.add(vectors)
-        self.metadata.extend(meta)
-        self._save()
+    def add(self, embedding, meta):
+        self.index.add(np.array([embedding], dtype=np.float32))
+        self.metadata.append(meta)
 
     def search(self, query_vector, top_k=5):
         query_vector = np.array([query_vector]).astype('float32')
