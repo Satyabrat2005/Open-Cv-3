f6e6ec70bf352bce4f4238a83f4b148403d66b8a
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-15 23:03:51 +0530

    Refactor object detection into ObjectDetector class
    Refactored object detection logic into ObjectDetector class.

--- VisionIq/src/object_detector.py
+++ VisionIq/src/object_detector.py
@@ -1,12 +1,24 @@
-from ultralytics import YOLO
-model = YOLO("models/yolov8n.pt")
+from ultralytics import YOLO  # type: ignore
 
-def detetct_objects(frame_path):
-    results = model.predict(frame_path)
-    objects = []
-    for r in results:
-        for box in r.boxes:
-            label = model.names[int(box.cls[0])]
-            confidence = float(box.conf[0])
-            objects.append({"label": label, "confidence": confidence})
-    return objects
+class ObjectDetector:
+    def __init__(self, model_path="yolov8n.pt"):
+        """
+        Initialize YOLOv8 object detector.
+        Default model: yolov8n.pt (lightweight and fast)
+        """
+        self.model = YOLO(model_path)
+
+    def detect_objects(self, frame_path):
+        """
+        Runs YOLOv8 detection on a single frame and returns detected objects.
+        """
+        results = self.model(frame_path)
+        objects = []
+
+        for r in results:
+            for box in r.boxes:
+                label = self.model.names[int(box.cls[0])]
+                confidence = float(box.conf[0])
+                objects.append({"label": label, "confidence": confidence})
+
+        return objects
