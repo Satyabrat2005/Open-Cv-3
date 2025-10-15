46be5ac0074fc5e1524e2b60650fe579298523db
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-15 15:54:28 +0530

    setup visioniq base

--- VisionIq/src/VisionIq.py
+++ VisionIq/src/VisionIq.py
@@ -1,1 +1,5 @@
-
+import os 
+from src.video_processor import extract_frames
+from src.object_detector import detect_objects
+from src.embedder import get_embedding
+from src.database import VectorDB
