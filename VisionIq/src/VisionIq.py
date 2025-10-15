d73d950a68432a878837ec8de4c1bab6263f637b
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-15 16:13:01 +0530

    updated main engine

--- VisionIq/src/VisionIq.py
+++ VisionIq/src/VisionIq.py
@@ -7,3 +7,14 @@ from src.database import VectorDB
 video_file = "data/videos/sample.mp4"
 frames_dir = "data/frames/sample"
 extract_frames(video_file, frames_dir, fps=1)
+
+db = VectorDB(dim=512)  # CLIP ViT-B/32 embedding size
+
+for frame_file in sorted(os.listdir(frames_dir)):
+    frame_path = os.path.join(frames_dir, frame_file)
+    objects = detect_objects(frame_path)
+    embedding = get_embedding(frame_path)
+    meta = {"frame": frame_path, "objects": objects}
+    db.add(embedding[0], meta) 
+db.save()
+print("Video processed and embeddings stored!")
