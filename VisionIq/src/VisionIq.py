acb50580501480b9bc46a0cd9a1fb887e41a5e0f
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-15 16:11:00 +0530

    Update VisionIq.py

--- VisionIq/src/VisionIq.py
+++ VisionIq/src/VisionIq.py
@@ -3,3 +3,7 @@ from src.video_processor import extract_frames
 from src.object_detector import detect_objects
 from src.embedder import get_embedding
 from src.database import VectorDB
+
+video_file = "data/videos/sample.mp4"
+frames_dir = "data/frames/sample"
+extract_frames(video_file, frames_dir, fps=1)
