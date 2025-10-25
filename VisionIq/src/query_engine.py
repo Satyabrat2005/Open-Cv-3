7771fd26337ad5e0368af50abf920fc141038737
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-25 09:48:29 +0530

    Implement search method in QueryEngine
    Add search method to QueryEngine for querying the database.

--- VisionIq/src/query_engine.py
+++ VisionIq/src/query_engine.py
@@ -5,3 +5,8 @@ class QueryEngine:
     def __init__(self, db):
         self.db = db
         self.text_encoder = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def search(self, query_text, top_k=3):
+            query_vec = self.text_encoder.encode([query_text])[0].astype(np.float32)
+            results = self.db.search(query_vec, top_k)
+            return results
