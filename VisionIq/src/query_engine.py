fce00aea6480c512c9e8cbbcd94f6ed20413a203
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-10-25 09:48:03 +0530

    Add QueryEngine class for handling queries

--- VisionIq/src/query_engine.py
+++ VisionIq/src/query_engine.py
@@ -1,2 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
+
+class QueryEngine:
+    def __init__(self, db):
+        self.db = db
+        self.text_encoder = SentenceTransformer("all-MiniLM-L6-v2")
