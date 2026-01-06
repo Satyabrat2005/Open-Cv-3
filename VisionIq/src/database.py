a578ee17154860724f44855a1b1bddf8d9ade912
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-06 12:48:24 +0530

    Update database.py

--- VisionIq/src/database.py
+++ VisionIq/src/database.py
@@ -2,3 +2,11 @@ import faiss
 import numpy as np 
 import os
 import pickle
+
+class VectorDatabase:
+    def __init__(self, dim=512, db_path="../data/embeddings"):
+        """
+        FAISS vector database for VisionIQ
+        dim     : embedding dimension (CLIP = 512)
+        db_path : where index + metadata are stored
+        """
