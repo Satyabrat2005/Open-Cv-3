73d0d9d0932bb88b2e7d2f323b2545b20cecefa8
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-07 19:40:40 +0530

    imported all useful libraries

--- VisionIq/src/query_engine.py
+++ VisionIq/src/query_engine.py
@@ -1,12 +1,7 @@
-from sentence_transformers import SentenceTransformer
 import numpy as np
+from embedder import ClipEmbedder
+from database import VectorDatabase
+
 
 class QueryEngine:
-    def __init__(self, db):
-        self.db = db
-        self.text_encoder = SentenceTransformer("all-MiniLM-L6-v2")
-
-    def search(self, query_text, top_k=3):
-            query_vec = self.text_encoder.encode([query_text])[0].astype(np.float32)
-            results = self.db.search(query_vec, top_k)
-            return results
+    def __init__(self, top_k=5):
