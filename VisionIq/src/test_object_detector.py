8fa3015774b15e010352fae86033534de9421290
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-09 12:44:08 +0530

    Update test_object_detector.py

--- VisionIq/src/test_object_detector.py
+++ VisionIq/src/test_object_detector.py
@@ -1,14 +1,9 @@
-from object_detector import ObjectDetector
 import os
-import cv2
+from collections import defaultdict
+from object_detector import ObjectDetector
 
-detector = ObjectDetector()
-
-frame_path = "../data/frames/test/frame_001.jpg"
-if not os.path.exists(frame_path):
-    print("Frame not found:", frame_path)
-else:
-    objects = detector.detect_objects(frame_path)
-    print("Detected objects:")
-    for obj in objects:
-        print(obj)
+# Initialize detector
+detector = ObjectDetector(
+    model_path="yolov8n.pt",  # you can change to yolov8s.pt later
+    conf=0.15                 # lower confidence to catch laptop etc.
+)
