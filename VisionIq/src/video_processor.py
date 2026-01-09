37b716c9599946c9dbba3e475b05735a8d08feae
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-09 16:49:04 +0530

    Error cap integrated

--- VisionIq/src/video_processor.py
+++ VisionIq/src/video_processor.py
@@ -5,3 +5,7 @@ def extract_frames(video_path, output_dir, frame_rate=1):
     os.makedirs(output_dir, exist_ok=True)
 
     cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print(f"[ERROR] Cannot open video: {video_path}")
+        return 0
