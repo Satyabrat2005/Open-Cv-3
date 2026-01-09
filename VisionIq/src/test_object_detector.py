f243c7f5bcf38491f4bb01993343c99e7c58d4e3
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-09 13:04:50 +0530

    Update test_object_detector.py

--- VisionIq/src/test_object_detector.py
+++ VisionIq/src/test_object_detector.py
@@ -7,3 +7,11 @@ detector = ObjectDetector(
     model_path="yolov8n.pt",  # you can change to yolov8s.pt later
     conf=0.15                 # lower confidence to catch laptop etc.
 )
+
+frames_dir = "../data/frames/test"
+
+# Get all frame files
+frames = sorted([
+    f for f in os.listdir(frames_dir)
+    if f.endswith(".jpg")
+])
