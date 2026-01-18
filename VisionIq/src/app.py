0b81df0c708b0f4e2e2f2b922b4e6d3d31f0081d
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-18 22:00:41 +0530

    Update app.py

--- VisionIq/src/app.py
+++ VisionIq/src/app.py
@@ -8,3 +8,11 @@ st.set_page_config(
     page_icon="🧠",
     layout="wide"
 )
+
+st.title("🧠 VisionIQ")
+st.subheader("Ask your video anything")
+
+if "chat" not in st.session_state:
+    st.session_state.chat = []
+
+question = st.text_input("Ask a question about the video")
