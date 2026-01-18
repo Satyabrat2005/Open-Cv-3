225f3a5874c807d3bb0881199409b3609b47f285
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-18 20:13:05 +0530

    Update test_llm_engine.py

--- VisionIq/src/test_llm_engine.py
+++ VisionIq/src/test_llm_engine.py
@@ -3,3 +3,14 @@ from llm_engine import LLMEngine
 engine = LLMEngine(model="deepseek-r1")
 
 evidence = [
+  {
+        "frame_id": "frame_00004.jpg",
+        "timestamp": 3.2,
+        "objects": ["bottle", "table"]
+    },
+    {
+        "frame_id": "frame_00007.jpg",
+        "timestamp": 5.8,
+        "objects": ["laptop", "backpack"]
+    }
+]
