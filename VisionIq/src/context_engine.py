65e2c705b2936c33fa23107e8eff75b1864b80e1
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-02-02 23:10:09 +0530

    Update context_engine.py

--- VisionIq/src/context_engine.py
+++ VisionIq/src/context_engine.py
@@ -1,2 +1,19 @@
 class ContextEngine:
-  def build_context(self,frame_meta, all_frame, window=3)
+    def build_context(self, frame_meta, all_frames, window=3):
+        ts = frame_meta["timestamp"]
+
+        before = [
+            f for f in all_frames
+            if ts - window <= f["meta"]["timestamp"] < ts
+        ]
+
+        after = [
+            f for f in all_frames
+            if ts < f["meta"]["timestamp"] <= ts + window
+        ]
+
+        return {
+            "current": frame_meta,
+            "before": before,
+            "after": after
+        }
