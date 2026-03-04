ad10d83bd010dd11fd3a357606f4e3f373718231
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-03-05 00:55:05 +0530

    Update video_memory.py

--- VisionIq/src/video_memory.py
+++ VisionIq/src/video_memory.py
@@ -2,3 +2,11 @@
 import os
 import pickle
 from collections import defaultdict
+
+class VideoMemoryEngine:
+    """
+    Builds structured memory of the entire video
+    from indexed frame metadata.
+
+    This is executed AFTER indexing.
+    """
