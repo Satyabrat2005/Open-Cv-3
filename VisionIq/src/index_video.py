cc56e2016d5e865ab603425289279dfe1aaf0027
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-03-10 23:59:43 +0530

    Update index_video.py

--- VisionIq/src/index_video.py
+++ VisionIq/src/index_video.py
@@ -4,6 +4,8 @@ import cv2
 from object_detector import ObjectDetector
 from embedder import ClipEmbedder
 from database import VectorDatabase
+from video_memory import VideoMemoryEngine
+
 
 FRAMES_DIR = "../data/frames/test"
 VIDEO_ID = "video_01"
